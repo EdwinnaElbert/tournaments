838945252b31c637414f38993a5570db8a6750d4
Edwinna <nalcarya1881@gmail.com>
2018-08-23 16:51:24 +0300

    new survey

--- spec/acceptance/my_surveys/create_survey.rb
+++ spec/acceptance/my_surveys/create_survey.rb
@@ -1,13 +1,24 @@
 
 require "feature_helper"
 
-feature "Signed in user creates a survey" do
+feature "Signed in user who has already one survey creates a survey" do
   let!(:user) { FactoryBot.create(:user) }
-  before(:each) do
-    sign_in(user)
-    click_on 'Create survey'
-  end
+  let!(:survey) { FactoryBot.create(:survey, user: user) }
+
   scenario "with valid email and password" do
-    fill_in 'survey_title', with: 'New survey title'
+    sign_in(user)
+
+    expect(page).to have_content('Create Survey')
+    # click_on 'add_survey'
+
+    visit new_my_survey_path
+    expect(current_path).to eq new_my_survey_path
+    expect(page).to have_content('Description')
+    fill_in 'survey[title]', with: 'New survey title'
+    fill_in 'survey_description', with: 'Lorem Ipsum'
+    check 'survey_is_anonymous'
+    click_on 'Add Only answer'
   end
 end
+
+# TODO user who hasn't git surveys creates a survey
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
@@ -7,6 +6,9 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+# frozen_string_literal: true
+#
+
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -69,3 +71,6 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+
+# Capybara::Screenshot.autosave_on_failure = false
