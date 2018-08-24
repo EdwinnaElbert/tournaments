b62c5e8b65034bffd20d5fad294840b47c82a500
Ilhom Madrahimov <madrahimov.ilhom@gmail.com>
2018-08-24 17:50:31 +0300

    move master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,27 @@
+# frozen_string_literal: true
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
+set :rails_env, "production"
+set :application, "survey"
+set :repo_url, "ssh://git@git.jowi.club:300/maryna/surveys.git"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :default_stage, "production"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :log_level, :info
+set :pty, true
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :linked_dirs, %w(log tmp vendor/bundle public/system public/assets)
+set :linked_files,        %w{config/database.yml config/master.key}
+# set :linked_files,        %w{config/database.yml config/redis.yml .env config/master.key}
+set :rvm_ruby_version,    "2.5.1@shopapi"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :bundle_env_variables, rails_env: "production"
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+after "deploy:publishing", "deploy:restart"
+namespace :deploy do
+  task :restart do
+    invoke "unicorn:stop"
+    invoke "unicorn:start"
+  end
+end
