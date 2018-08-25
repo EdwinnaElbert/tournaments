9c28a966781559f9ad21e729e83a341a0d63a5fd
Edwinna <nalcarya1881@gmail.com>
2018-08-25 21:09:40 +0300

    add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.action_mailer_default_url_options = {host: "my.host.com"}
+  config.action_mailer_default_url_options = {host: "survey.toqomo.com"}
   config.action_mailer_smtp_settings = {
     enable_starttls_auto: false,
     domain: "survey.toqomo.com",
