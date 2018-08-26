faca2c9890e84d36e33991081ec5134ac5b1ad57
Edwinna <nalcarya1881@gmail.com>
2018-08-26 12:42:39 +0300

    bundle install

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,9 @@ Rails.application.config.assets.precompile += %w( scripts.bundle.js )
 Rails.application.config.assets.precompile += %w( vendors.bundle.js )
 
 Rails.application.config.assets.precompile += %w( vendors.bundle.css )
-# Rails.application.config.assets.paths << Rails.root.join("fonts")
+Rails.application.config.assets.paths << Rails.root.join("fonts/flaticon/")
+Rails.application.config.assets.paths << Rails.root.join("fonts/fontawesome5/")
+Rails.application.config.assets.paths << Rails.root.join("fonts/line-awesome/")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
