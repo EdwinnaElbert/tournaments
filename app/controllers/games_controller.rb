9a7d24ccae4b6305b4c6e56eae06fec9e838590d
Edwinna <nalcarya1881@gmail.com>
2018-10-02 10:33:26 +0300

    move logic to aasm_states and services

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,10 +5,7 @@ class GamesController < AppController
   before_action :set_tournament
 
   def create
-    unless @tournament.aasm_state == "final"
-      @tournament.to_next_state!
-      ArrangeMatchesService.new(@tournament)
-    end
+    @tournament.to_next_state! unless @tournament.aasm_state == "final"
     redirect_to tournament_path(@tournament)
   end
 
