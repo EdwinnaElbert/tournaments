5f78927d8ba56fe55b13c6a5d15135e8461f356b
Edwinna <nalcarya1881@gmail.com>
2018-10-03 20:23:01 +0300

    rubocop fixes

--- app/services/divide_winners_loosers_service.rb
+++ app/services/divide_winners_loosers_service.rb
@@ -10,7 +10,7 @@ class DivideWinnersLoosersService
       scores << match.scores
     end
 
-    team_total_scores = scores.flatten.map { |s| [s.team_id, s.score] }.group_by { |t| t[0] }.map{ |t| [t[1].map {|tt| tt[1]}.flatten.sum, t[0]] }.sort!
+    team_total_scores = scores.flatten.map { |s| [s.team_id, s.score] }.group_by { |t| t[0] }.map { |t| [t[1].map { |tt| tt[1] }.flatten.sum, t[0]] }.sort!
     loosers << team_total_scores.slice(0..((team_total_scores.count / 2) - 1))
     winners_scores << team_total_scores.slice((team_total_scores.count / 2)..(team_total_scores.count - 1))
 
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Surveys
@@ -16,7 +16,7 @@ module Surveys
     # config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.i18n.default_locale = :ru
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**/*.{rb,yml}").to_s]
 
     config.i18n.available_locales = [:en, :ru]
     # Settings in config/environments/* take precedence over those specified here.
