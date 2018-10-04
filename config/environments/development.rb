ebb9b99e7a8e7501c34a1add211ff3e6e598389e
Edwinna <nalcarya1881@gmail.com>
2018-10-04 19:17:03 +0300

    disable assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,13 +56,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # --------
-  config.serve_static_assets = false
-  config.assets.js_compressor = :uglifier
-  config.assets.compile = false
-  config.assets.digest = true
-  config.assets.debug = false
-  # -------
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
