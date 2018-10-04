395f827393ce627c137eba95dce1a8f5fe05ae7e
Edwinna <nalcarya1881@gmail.com>
2018-10-04 19:20:47 +0300

    disable assets

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Tournaments
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.enabled = true
+    config.assets.enabled = false
     # config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.i18n.default_locale = :ru
