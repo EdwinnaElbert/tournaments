58309a349f482001a9d1c7582a05fb6bd51099e1
Edwinna <nalcarya1881@gmail.com>
2018-10-05 12:10:33 +0300

    migration commented

--- db/migrate/20180929125906_create_teams.rb
+++ db/migrate/20180929125906_create_teams.rb
@@ -1,14 +1,14 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
-  def up
-    create_table :teams, id: :uuid, default: "uuid_generate_v1()" do |t|
-      t.string     :title,      null: false
-      t.boolean    :off,        null: false, default: false
-
-      t.timestamps
-    end
-  end
-
-  def down
-    drop_table :teams
-  end
+  # def up
+  #   create_table :teams, id: :uuid, default: "uuid_generate_v1()" do |t|
+  #     t.string     :title,      null: false
+  #     t.boolean    :off,        null: false, default: false
+  #
+  #     t.timestamps
+  #   end
+  # end
+  #
+  # def down
+  #   drop_table :teams
+  # end
 end
--- db/migrate/20181003171231_add_winner_to_tournaments.rb
+++ db/migrate/20181003171231_add_winner_to_tournaments.rb
@@ -1,9 +1,9 @@
 class AddWinnerToTournaments < ActiveRecord::Migration[5.2]
-  def up
-    add_reference :tournaments, :winner, references: :teams, type: :uuid, foreign_key: { to_table: :teams }
-  end
-
-  def down
-    remove_reference :tournaments, :winner
-  end
+  # def up
+  #   add_reference :tournaments, :winner, references: :teams, type: :uuid, foreign_key: { to_table: :teams }
+  # end
+  #
+  # def down
+  #   remove_reference :tournaments, :winner
+  # end
 end
