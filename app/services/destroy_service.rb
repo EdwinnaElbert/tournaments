8969844d66d418b24316b4ac705d184b55ef7a72
Edwinna <nalcarya1881@gmail.com>
2018-10-05 13:47:00 +0300

    destroy

--- app/services/destroy_service.rb
+++ app/services/destroy_service.rb
@@ -2,9 +2,8 @@ class DestroyService
   def self.call
     Score.destroy_all
     Match.destroy_all
-    Team.destroy_all
-
     Group.destroy_all
     Tournament.destroy_all
+    Team.destroy_all
   end
 end
