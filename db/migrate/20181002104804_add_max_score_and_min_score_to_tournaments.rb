a66a9ff555133d543a74c6b1d38815d03c186bc8
Edwinna <nalcarya1881@gmail.com>
2018-10-05 12:49:26 +0300

    migration commented

--- db/migrate/20181002104804_add_max_score_and_min_score_to_tournaments.rb
+++ db/migrate/20181002104804_add_max_score_and_min_score_to_tournaments.rb
@@ -1,11 +1,11 @@
-# class AddMaxScoreAndMinScoreToTournaments < ActiveRecord::Migration[5.2]
-#   def up
-#     add_column :tournaments, :min_score, :integer, null: false, default: 0
-#     add_column :tournaments, :max_score, :integer, null: false, default: 100
-#   end
-#
-#   def down
-#     remove_column :tournaments, :min_score
-#     remove_column :tournaments, :max_score
-#   end
-# end
+class AddMaxScoreAndMinScoreToTournaments < ActiveRecord::Migration[5.2]
+  def up
+    add_column :tournaments, :min_score, :integer, null: false, default: 0
+    add_column :tournaments, :max_score, :integer, null: false, default: 100
+  end
+
+  def down
+    remove_column :tournaments, :min_score
+    remove_column :tournaments, :max_score
+  end
+end
